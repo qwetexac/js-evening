188f5008f5fea043fe831e25304cc3f3d540dfe6
a.vorobyov <a.vorobyov@semrush.com>
2020-09-05 16:56:53 +0300

    Updates

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@ var __wpo = {
       "/"
     ],
     "additional": [
-      "/main.67b35dd2979b9ec8c069.chunk.js",
+      "/main.6e32afe0a8b7956e8786.chunk.js",
       "/npm.babel.479115b2542204282bcd.chunk.js",
       "/npm.emotion.50b0a71fe1806ed3fcea.chunk.js",
       "/npm.hoist-non-react-statics.ec551dc46829f381ff37.chunk.js",
@@ -20,7 +20,7 @@ var __wpo = {
   },
   "externals": [],
   "hashesMap": {
-    "804f438706fc1d7ddc9791345a4cb69d0e01461c": "/main.67b35dd2979b9ec8c069.chunk.js",
+    "99a169f37a1313957cae0845d1f71a8de9a5048c": "/main.6e32afe0a8b7956e8786.chunk.js",
     "0aea35742c2face8f72fbf0b50b36fd0939bcbee": "/npm.babel.479115b2542204282bcd.chunk.js",
     "d438d55caf830f909e91749c381baabd6d21362d": "/npm.emotion.50b0a71fe1806ed3fcea.chunk.js",
     "e61ad7b2d03f904c4ad89c6d64d21915f0e7f932": "/npm.hoist-non-react-statics.ec551dc46829f381ff37.chunk.js",
@@ -31,11 +31,11 @@ var __wpo = {
     "78531da95230bc1f9bac80f23cdee79550b01462": "/npm.prop-types.de1f7f412ae540dc3465.chunk.js",
     "8e4f710cb68832a9bb8c2e24464ea6560e2142b1": "/npm.react-app-polyfill.a512adaad14151170c55.chunk.js",
     "fa96499c0f20a94e74155b77149ccd42fb04da05": "/runtime.ea2ca37bb4a5c348357c.js",
-    "d5dbf62c8490a59eb7c7c6b22381ad465fb99927": "/"
+    "9711cab5dd27d9c37c37b818b45f38cd06ced6f3": "/"
   },
   "strategy": "changed",
   "responseStrategy": "cache-first",
-  "version": "2020-9-5 16:47:54",
+  "version": "2020-9-5 16:56:51",
   "name": "webpack-offline",
   "pluginVersion": "5.0.6",
   "relativePaths": false
